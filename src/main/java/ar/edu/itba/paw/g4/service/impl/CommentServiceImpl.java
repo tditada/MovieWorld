ce4ea2ae646148ece71f8a39908fc49ed28b76cf
kpjjpk <juanpablorey.10@gmail.com>
2014-09-15 03:25:16 -0300

    Done. Everything working

---
+++ src/main/java/ar/edu/itba/paw/g4/service/impl/CommentServiceImpl.java
@@ -0,0 +1,32 @@
+package ar.edu.itba.paw.g4.service.impl;
+
+import static ar.edu.itba.paw.g4.util.validation.PredicateHelpers.notNull;
+import static ar.edu.itba.paw.g4.util.validation.Validations.checkArgument;
+
+import java.util.List;
+
+import ar.edu.itba.paw.g4.exception.DatabaseException;
+import ar.edu.itba.paw.g4.exception.ServiceException;
+import ar.edu.itba.paw.g4.model.Comment;
+import ar.edu.itba.paw.g4.model.User;
+import ar.edu.itba.paw.g4.persist.CommentDAO;
+import ar.edu.itba.paw.g4.persist.impl.PSQLCommentDAO;
+import ar.edu.itba.paw.g4.service.CommentService;
+
+public class CommentServiceImpl implements CommentService {
+	private CommentDAO commentDAO = PSQLCommentDAO.getInstance();
+
+	@Override
+	public List<Comment> getCommentsOf(User user) {
+		checkArgument(user, notNull());
+		try {
+			return commentDAO.getAllByUser(user);
+		} catch (DatabaseException dbe) {
+			throw new ServiceException(dbe);/*
+											 * TODO: deberia chequear por otros
+											 * tipos de exception?
+											 */
+		}
+	}
+
+}
